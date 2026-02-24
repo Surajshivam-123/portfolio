37f8846aa383636c056bbf4be4f8f9daaf72f734
Surajshivam-123 <sj919810@gmail.com>
2026-02-24 23:56:05 +0530

    portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { writeFileSync } from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: 'create-nojekyll',
+      closeBundle() {
+        writeFileSync('dist/.nojekyll', '')
+      }
+    }
+  ],
   base: "/portfolio/",
 })
